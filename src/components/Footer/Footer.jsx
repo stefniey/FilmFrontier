fc590d2ac77f9776114120af521a8e9f79b108ba
stefniey <stephaniealor7@gmail.com>
2024-07-18 03:23:12 -0700

    h

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './footer.css'
+import './Footer.css'
 import { FaInstagram, FaFacebook, FaTwitter } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
 
