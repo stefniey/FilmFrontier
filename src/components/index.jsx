4e3e991e79a3eea1de6d182a693a19d3c2b4058b
stefniey <stephaniealor7@gmail.com>
2024-07-18 02:55:07 -0700

    new

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,8 @@
 import Navbar from './Navbar/Navbar';
 import SignUp from './SignUp/SignUp';
-import Login from './Login/Login'
-import Footer from './Footer/footer'
-import SearchResults from './SearchResults/SearchResults'
+import Login from './Login/Login';
+import Footer from './Footer/Footer';
+import SearchResults from './SearchResults/SearchResults';
 
 export{
     Navbar,
@@ -10,7 +10,4 @@ export{
     Login,
     SearchResults,
     Footer
-
-
-
 }
